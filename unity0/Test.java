8fba3f6c77b8c5cacc1ff670b6798997bd942652
AULA1 <AULA1@192.168.31.3>
2016-02-09 16:04:28 -0500

    stack test

---
+++ unity0/Test.java
@@ -0,0 +1,22 @@
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unity0;
+
+/**
+ *
+ * @author AULA1
+ */
+public class Test {
+   public static void main(String[] args){
+       Stack s = new Stack();
+       
+       s.add(10);
+       s.add(20);
+       s.add(30);
+       
+   } 
+}
